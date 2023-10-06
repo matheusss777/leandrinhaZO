0c51da8e0b436ccf49bf068a56535139bf71ef5f
matheusss777 <128159639+matheusss777@users.noreply.github.com>
2023-10-06 11:58:18 -0300

    Add files via upload

---
+++ matheus/cristiano.js
@@ -0,0 +1,18 @@
+function pedir(){
+    var opcao = prompt("Escolha uma opção de 1 a 4")
+    switch(Number(opcao)){
+        case 1:
+            alert("Voce escolheu Cristiano bolachinha")
+            break
+            case 2:
+            alert("Voce escolheu Cristiano bolinho")
+            break
+            case 3:
+            alert("Voce escolheu Cristiano tsrrrrr")
+            break
+            case 4:
+            alert("Voce escolheu Cristiano alfacinho")
+
+
+    }
+}
